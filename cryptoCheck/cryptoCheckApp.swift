c095dbcb61c7cb8ed9804dd7c06dc53b68a09f3a
Leonardo Soares <leonardofs88@live.com>
2025-10-25 12:41:30 +0100

    [WIP] App creation

---
+++ cryptoCheck/cryptoCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoCheckApp.swift
+//  cryptoCheck
+//
+//  Created by Leonardo Soares on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct cryptoCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
